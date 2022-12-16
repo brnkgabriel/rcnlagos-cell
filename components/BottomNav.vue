418000f020b46f8a7e9ccb9e305229cd65505885
brnkgabriel <brnkgabriel@gmail.com>
2022-12-16 21:22:10 +0100

    fix: bottom nav text

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -6,7 +6,7 @@
       <div :class="bottomNavLinkIcon">
         <Icon type="events" class="w-[24px] h-[24px]" :active="route.name === 'events'" />
       </div>
-      <div class="capitalize" :class="subline + ' ' + color('events')">events</div>
+      <div class="capitalize" :class="subline_small + ' ' + color('events')">events</div>
     </NuxtLink>
     <NuxtLink
       href="/meetings"
@@ -14,7 +14,7 @@
       <div :class="bottomNavLinkIcon">
         <Icon type="meetings" class="w-[24px] h-[24px]" :active="route.name === 'meetings'" />
       </div>
-      <div class="capitalize" :class="subline + ' ' + color('meetings')">meetings</div>
+      <div class="capitalize" :class="subline_small + ' ' + color('meetings')">meetings</div>
     </NuxtLink>
     <NuxtLink
       href="/members"
@@ -22,7 +22,7 @@
       <div :class="bottomNavLinkIcon">
         <Icon type="members" class="w-[24px] h-[24px]" :active="route.name === 'members'" />
       </div>
-      <div class="capitalize" :class="subline + ' ' + color('members')">members</div>
+      <div class="capitalize" :class="subline_small + ' ' + color('members')">members</div>
     </NuxtLink>
     <NuxtLink
       href="/settings"
@@ -30,13 +30,13 @@
       <div :class="bottomNavLinkIcon">
         <Icon type="settings" class="w-[24px] h-[24px]" :active="route.name === 'settings'" />
       </div>
-      <div class="capitalize" :class="subline + ' ' + color('settings')">settings</div>
+      <div class="capitalize" :class="subline_small + ' ' + color('settings')">settings</div>
     </NuxtLink>
   </div>
 </template>
 <script setup lang="ts">
 
-const { subline, bottomNavLink, bottomNavLinkIcon } = useUi()
+const { subline_small, bottomNavLink, bottomNavLinkIcon } = useUi()
 
 const route = useRoute()
 
