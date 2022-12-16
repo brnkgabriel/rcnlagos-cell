c79300ac686ecd67a8cbfc1afd3f597210b16356
brnkgabriel <brnkgabriel@gmail.com>
2022-12-16 21:09:19 +0100

    fix: homified circular logo

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="max-w-[576px] mx-auto h-full w-screen flex justify-center items-center font-body relative">
-    <div :class="logo">
+    <NuxtLink href="/" :class="logo">
       <img src="/icons/logo-colored.svg" alt="logo"/>
-    </div>
+    </NuxtLink>
     <slot></slot>
   </div>
 </template>
